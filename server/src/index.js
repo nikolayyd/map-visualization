d794449888abeaf6c280e3a5028d9bad70ed4df3
Nikolay Dragostinov <nikolayddragostinov@gmail.com>
2025-09-04 09:28:32 +0300

    added dotenv

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,18 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+app.use(cors());
+
+const dbHost = process.env.DB_HOST;
+const dbPort = Number(process.env.DB_PORT);
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+
 const PORT = 5000;
 
 const { Pool } = require("pg");
@@ -9,9 +20,6 @@ const { Pool } = require("pg");
 const pool = new Pool({
 
 });
-
-app.use(cors());
-app.use(express.json());
 
 app.get("...link", (req, res) => {
     res.json();
